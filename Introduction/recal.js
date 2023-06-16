88b5e55c90073dec32170acf0d95f112b5d81628
Olawalesegun <olawalesegun0@gmail.com>
2023-06-16 20:56:51 +0100

    Revisting Js

---
+++ Introduction/recal.js
@@ -0,0 +1,17 @@
+var number = 20;
+
+let age = 23;
+
+const SCHOOL = "Semicolon Africa";
+
+age = 25;
+
+// Re assigning a constant variable would not be possible
+// as this will throw a TypeErro
+
+//SCHOOL = "jAGAGA"
+
+
+console.log(age);
+console.log(number);
+console.log(SCHOOL);
