3c87fbea653fa454b3c40b27c40845fae8d86b02
Olawalesegun <olawalesegun0@gmail.com>
2023-08-07 16:27:25 +0100

    index javascript file

---
+++ Introduction/dom/index.js
@@ -0,0 +1,37 @@
+var body = document.body;
+var firstChild = body.firstElementChild;
+var h1 = firstChild.firstElementChild;
+h1.innerHTML = "KuliTech Ventures"
+
+var kuliTech = document.body.getElementsByTagName("h1");
+kuliTech[0].innerText = "KuliTech Business";
+
+var kuliBusiness = document.getElementById("compName");
+kuliBusiness.innerHTML = "KuliTech Enterprise";
+
+let groceryRow = document.createElement(tr);
+let grocerydata = document.createElement(td);
+let grocery = "Garri";
+let price = "500";
+
+function addGroceries(table, grocery, price){
+    let groceryRow = document.createElement("tr");
+
+    let groceryData = document.createElement("td");
+    let priceData = document.createElement("td");
+
+    groceryData.innerHTML = grocery;
+    priceData.innerHTML = price;
+
+    grocery.appendChild(groceryData);
+    groceryRow.appendChild(priceData);
+
+    table.childNodes[1].appendChild(groceryRow);
+}
+
+let priceTable = document.getElementById("priceTable");
+
+addGroceries(priceTable, "Mongo", 300);
+// console.log("First Child:", body.firstChild)
+// console.log(window.document.body)
+// console.log()
